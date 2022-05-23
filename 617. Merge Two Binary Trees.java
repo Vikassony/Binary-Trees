a3c1efb8ec8872c21298df494beba2e8c07cf178
Vikassony <48961830+Vikassony@users.noreply.github.com>
2022-05-23 19:52:14 +0530

    Create 617. Merge Two Binary Trees.java

---
+++ 617. Merge Two Binary Trees.java
@@ -0,0 +1,13 @@
+Question Link : https://leetcode.com/problems/merge-two-binary-trees/
+
+class Solution {
+    public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
+        if(root1==null || root2==null){
+            return root1!=null?root1:root2;
+        }
+        root1.val += root2.val;
+        root1.left = mergeTrees(root1.left,root2.left);
+        root1.right = mergeTrees(root1.right,root2.right);
+        return root1;
+    }
+}
